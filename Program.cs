1f5be869192f6a71e8cba04933e2366d90f31668
jayceezxc28 <mharjaycee@gmail.com>
2026-04-09 23:43:26 +0800

    Update Program.cs
    This commit creates a Product class with properties (Id, Name, Price, RemainingStock) and methods for displaying product data and calculating item totals, along with the initialization of a product array.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,72 @@
+using System;
+namespace quiz
+{
+    class Product
+    {
+        public int Id;
+        public string Name;
+        public double Price;
+        public int RemainingStock;
 
+        public void DisplayProduct()
+        {
+            Console.WriteLine($"{Id, -5} {Name, -15} {Price, -10} {RemainingStock, -10}");
+        }
+        public double GetItemTotal(int quantity)
+        {
+            return Price * quantity;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] product = new Product[] {
+            new Product
+            {
+                Id=1,
+                Name="Mouse",
+                Price=350,
+                RemainingStock=50
+            },
+            new Product
+            {
+                Id=2,
+                Name="Keyboard",
+                Price=800,
+                RemainingStock=30
+            },
+            new Product
+            {
+                Id=3,
+                Name="Headset",
+                Price=1200,
+                RemainingStock=20
+            },
+            new Product
+            {
+                Id=4,
+                Name="Flash Drive",
+                Price=500,
+                RemainingStock=15
+            },
+            new Product
+            {
+                Id=5,
+                Name="Monitor",
+                Price=6500,
+                RemainingStock=10
+            }
+            };
+
+            Console.WriteLine("ID    Name            Price      Remaining Stock");
+
+            for(int a = 0; a<product.Length; a++)
+            {
+                product[a].DisplayProduct();
+            }
+
+        }
+    }
+}
